004fa55ec667aa739241e768ed69a1e6b47a9381
Trump7 <ma332651@ucf.edu>
2024-07-09 14:56:32 -0400

    fixed scrollbar complelty, changed layout of dashboard

--- webapp/wrench-monkey/src/Pages/UserDashboard.jsx
+++ webapp/wrench-monkey/src/Pages/UserDashboard.jsx
@@ -6,8 +6,8 @@ import '../index.css';
 
 const UserDashboard = () => {
     return (
-        <div className="bg-[#00001B] min-h-screen flex flex-col items-center pt-20 pb-20">
-            <div className="flex justify-between items-start space-x-8 w-full max-w-6xl">
+        <div className="bg-[#00001B] min-h-screen flex flex-col items-center pt-5 pb-20">
+            <div className="flex justify-between items-start space-x-8 w-full max-w-7xl">
                 <div className="bg-gray-500 p-4 rounded-lg w-1/3 h-screen">
                     <Tools admin={false} /> {/* Pass admin prop to disable edit/delete/add */}
                 </div>
